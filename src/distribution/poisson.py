a7711ba12068a342c9d2148f8db78b4a317f7341
luhuan wu <lhwunju@outlook.com>
2019-08-12 23:33:03 -0400

    add sample in mvn and dirichlet

--- src/distribution/poisson.py
+++ src/distribution/poisson.py
@@ -3,7 +3,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow_probability import distributions as tfd
 
-from distribution.base import distribution
+from src.distribution.base import distribution
 
 
 class poisson(distribution):
