ada404155f0370b2c231ff783d9346a1a2a8b46c
Zizhao Wang <zzwang@umich.edu>
2019-08-27 11:43:26 -0400

    add new data to available_data.py

--- src/utils/available_data.py
+++ src/utils/available_data.py
@@ -35,6 +35,13 @@ clv_count_diff_length_1000_dir = "data/clv/clv_count_diff_length_1000.p"
 
 clv_count_dir = "data/count_clv.p"
 
+bootstrap_600 = "data/bootstrap/bootstrap_600.p"
+bootstrap_600_w_noise = "data/bootstrap/bootstrap_600_w_noise.p"
+train_0_purple_repeat_600 = "data/bootstrap/train_0_purple_repeat_600.p"
+train_0_purple_repeat_600_w_noise = "data/bootstrap/train_0_purple_repeat_600_w_noise.p"
+train_8_pink_repeat_600 = "data/bootstrap/train_8_pink_repeat_600.p"
+train_8_pink_repeat_600_w_noise = "data/bootstrap/train_8_pink_repeat_600_w_noise.p"
+
 DATA_DIR_DICT = dict(toy=toy_data_dir, percentage=percentage_data_dir,
                      count=count_data_dir,
                      percentage_noinputs=percentage_noinputs_dir,
@@ -52,11 +59,19 @@ DATA_DIR_DICT = dict(toy=toy_data_dir, percentage=percentage_data_dir,
                      clv_count_diff_length_200=clv_count_diff_length_200_dir,
                      clv_count_diff_length_600=clv_count_diff_length_600_dir,
                      clv_count_diff_length_1000=clv_count_diff_length_1000_dir,
-                     )
+                     bootstrap_600=bootstrap_600_dir,
+                     bootstrap_600_w_noise=bootstrap_600_w_noise_dir,
+                     train_0_purple_repeat_600=train_0_purple_repeat_600_dir,
+                     train_0_purple_repeat_600_w_noise=train_0_purple_repeat_600_w_noise_dir,
+                     train_8_pink_repeat_600=train_8_pink_repeat_600_dir,
+                     train_8_pink_repeat_600_w_noise=train_8_pink_repeat_600_w_noise_dir)
 
 PERCENTAGE_DATA_TYPE = ["percentage", "percentage_noinputs", "clv", "clv_08", "clv_06", "clv_05", "clv_04",
                         "clvi", "clvi_08", "clvi_06", "clvi_05", "clvi_04", "clv_input_noise", "clv_input_noise_1000",
-                        "clv_diff_length_200", "clv_diff_length_600", "clv_diff_length_1000"]
+                        "clv_diff_length_200", "clv_diff_length_600", "clv_diff_length_1000",
+                        "bootstrap_600", "bootstrap_600_w_noise",
+                        "train_0_purple_repeat_600", "train_0_purple_repeat_600_w_noise",
+                        "train_8_pink_repeat_600", "train_8_pink_repeat_600_w_noise"]
 
 COUNT_DATA_TYPE = ["count", "count_noinputs", "pink_count", "cyan_count", "clv_count",
                    "clv_count_diff_length_200", "clv_count_diff_length_600", "clv_count_diff_length_1000"]
