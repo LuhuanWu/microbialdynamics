4cec1721a5a55ab8d1852cfdf08f3231fd08fff2
wangzizhao <zzwang@umich.edu>
2019-12-07 13:57:43 -0500

    clean unused clv data; improve clv simulation stability

--- src/utils/available_data.py
+++ src/utils/available_data.py
@@ -1,158 +1,28 @@
 # available data options
 toy_data_dir = "data/fhn_with_inputs_dirichlet"
 
+# --------------------------------------------- real data --------------------------------------------- #
 percentage_data_dir = "data/microbio.p"
 count_data_dir = "data/count_microbio.p"
 
-#k6
+# k6
 count_data_k2_dir = "data/count_microbio_k2.p"
 count_data_k6_dir = "data/count_microbio_k6.p"
 count_data_k8_dir = "data/count_microbio_k8.p"
 
-percentage_split_3_dir = "data/microbio_split_3.p"
-percentage_split_5_dir = "data/microbio_split_5.p"
-count_split_3_dir = "data/count_microbio_split_3.p"
-count_split_5_dir = "data/count_microbio_split_5.p"
-
 percentage_small_dir = "data/microbio_small.p"
 count_small_dir = "data/count_microbio_small.p"
 
-# percentage, scale = 1
-clv_percentage_Dx_1_scale_1_data_dir = "data/clv/data/clv_percentage_Dx_1_scale_1.p"
-clv_percentage_Dx_2_scale_1_data_dir = "data/clv/data/clv_percentage_Dx_2_scale_1.p"
-clv_percentage_Dx_3_scale_1_data_dir = "data/clv/data/clv_percentage_Dx_3_scale_1.p"
-clv_percentage_Dx_4_scale_1_data_dir = "data/clv/data/clv_percentage_Dx_4_scale_1.p"
-clv_percentage_Dx_5_scale_1_data_dir = "data/clv/data/clv_percentage_Dx_5_scale_1.p"
-clv_percentage_Dx_6_scale_1_data_dir = "data/clv/data/clv_percentage_Dx_6_scale_1.p"
-clv_percentage_Dx_7_scale_1_data_dir = "data/clv/data/clv_percentage_Dx_7_scale_1.p"
-clv_percentage_Dx_8_scale_1_data_dir = "data/clv/data/clv_percentage_Dx_8_scale_1.p"
-clv_percentage_Dx_9_scale_1_data_dir = "data/clv/data/clv_percentage_Dx_9_scale_1.p"
-clv_percentage_Dx_10_scale_1_data_dir = "data/clv/data/clv_percentage_Dx_10_scale_1.p"
+# ---------------------------------------------- clv data --------------------------------------------- #
+clv_count_Dx10_obs02_dir = "data/clv/data/clv_count_ntrain_200_Dx_10_obs_02.p"
+clv_count_Dx10_obs06_dir = "data/clv/data/clv_count_ntrain_200_Dx_10_obs_06.p"
+clv_count_Dx10_obs10_dir = "data/clv/data/clv_count_ntrain_200_Dx_10_obs_10.p"
 
-# percentage, scale = 4
-clv_percentage_Dx_1_scale_4_data_dir = "data/clv/data/clv_percentage_Dx_1_scale_4.p"
-clv_percentage_Dx_2_scale_4_data_dir = "data/clv/data/clv_percentage_Dx_2_scale_4.p"
-clv_percentage_Dx_3_scale_4_data_dir = "data/clv/data/clv_percentage_Dx_3_scale_4.p"
-clv_percentage_Dx_4_scale_4_data_dir = "data/clv/data/clv_percentage_Dx_4_scale_4.p"
-clv_percentage_Dx_5_scale_4_data_dir = "data/clv/data/clv_percentage_Dx_5_scale_4.p"
-clv_percentage_Dx_6_scale_4_data_dir = "data/clv/data/clv_percentage_Dx_6_scale_4.p"
-clv_percentage_Dx_7_scale_4_data_dir = "data/clv/data/clv_percentage_Dx_7_scale_4.p"
-clv_percentage_Dx_8_scale_4_data_dir = "data/clv/data/clv_percentage_Dx_8_scale_4.p"
-clv_percentage_Dx_9_scale_4_data_dir = "data/clv/data/clv_percentage_Dx_9_scale_4.p"
-clv_percentage_Dx_10_scale_4_data_dir = "data/clv/data/clv_percentage_Dx_10_scale_4.p"
-
-# count, scale = 1
-clv_count_Dx_1_scale_1_data_dir = "data/clv/data/clv_count_Dx_1_scale_1.p"
-clv_count_Dx_2_scale_1_data_dir = "data/clv/data/clv_count_Dx_2_scale_1.p"
-clv_count_Dx_3_scale_1_data_dir = "data/clv/data/clv_count_Dx_3_scale_1.p"
-clv_count_Dx_4_scale_1_data_dir = "data/clv/data/clv_count_Dx_4_scale_1.p"
-clv_count_Dx_5_scale_1_data_dir = "data/clv/data/clv_count_Dx_5_scale_1.p"
-clv_count_Dx_6_scale_1_data_dir = "data/clv/data/clv_count_Dx_6_scale_1.p"
-clv_count_Dx_7_scale_1_data_dir = "data/clv/data/clv_count_Dx_7_scale_1.p"
-clv_count_Dx_8_scale_1_data_dir = "data/clv/data/clv_count_Dx_8_scale_1.p"
-clv_count_Dx_9_scale_1_data_dir = "data/clv/data/clv_count_Dx_9_scale_1.p"
-clv_count_Dx_10_scale_1_data_dir = "data/clv/data/clv_count_Dx_10_scale_1.p"
-
-# count, scale = 4
-clv_count_Dx_1_scale_4_data_dir = "data/clv/data/clv_count_Dx_1_scale_4.p"
-clv_count_Dx_2_scale_4_data_dir = "data/clv/data/clv_count_Dx_2_scale_4.p"
-clv_count_Dx_3_scale_4_data_dir = "data/clv/data/clv_count_Dx_3_scale_4.p"
-clv_count_Dx_4_scale_4_data_dir = "data/clv/data/clv_count_Dx_4_scale_4.p"
-clv_count_Dx_5_scale_4_data_dir = "data/clv/data/clv_count_Dx_5_scale_4.p"
-clv_count_Dx_6_scale_4_data_dir = "data/clv/data/clv_count_Dx_6_scale_4.p"
-clv_count_Dx_7_scale_4_data_dir = "data/clv/data/clv_count_Dx_7_scale_4.p"
-clv_count_Dx_8_scale_4_data_dir = "data/clv/data/clv_count_Dx_8_scale_4.p"
-clv_count_Dx_9_scale_4_data_dir = "data/clv/data/clv_count_Dx_9_scale_4.p"
-clv_count_Dx_10_scale_4_data_dir = "data/clv/data/clv_count_Dx_10_scale_4.p"
-
-### no inputs
-
-# percentage, scale = 1
-clv_noinput_percentage_Dx_1_scale_1_data_dir = "data/clv/data_noinput/clv_percentage_Dx_1_scale_1.p"
-clv_noinput_percentage_Dx_2_scale_1_data_dir = "data/clv/data_noinput/clv_percentage_Dx_2_scale_1.p"
-clv_noinput_percentage_Dx_3_scale_1_data_dir = "data/clv/data_noinput/clv_percentage_Dx_3_scale_1.p"
-clv_noinput_percentage_Dx_4_scale_1_data_dir = "data/clv/data_noinput/clv_percentage_Dx_4_scale_1.p"
-clv_noinput_percentage_Dx_5_scale_1_data_dir = "data/clv/data_noinput/clv_percentage_Dx_5_scale_1.p"
-clv_noinput_percentage_Dx_6_scale_1_data_dir = "data/clv/data_noinput/clv_percentage_Dx_6_scale_1.p"
-clv_noinput_percentage_Dx_7_scale_1_data_dir = "data/clv/data_noinput/clv_percentage_Dx_7_scale_1.p"
-clv_noinput_percentage_Dx_8_scale_1_data_dir = "data/clv/data_noinput/clv_percentage_Dx_8_scale_1.p"
-clv_noinput_percentage_Dx_9_scale_1_data_dir = "data/clv/data_noinput/clv_percentage_Dx_9_scale_1.p"
-clv_noinput_percentage_Dx_10_scale_1_data_dir = "data/clv/data_noinput/clv_percentage_Dx_10_scale_1.p"
-
-# count, scale = 1
-clv_noinput_count_Dx_1_scale_1_data_dir = "data/clv/data_noinput/clv_count_Dx_1_scale_1.p"
-clv_noinput_count_Dx_2_scale_1_data_dir = "data/clv/data_noinput/clv_count_Dx_2_scale_1.p"
-clv_noinput_count_Dx_3_scale_1_data_dir = "data/clv/data_noinput/clv_count_Dx_3_scale_1.p"
-clv_noinput_count_Dx_4_scale_1_data_dir = "data/clv/data_noinput/clv_count_Dx_4_scale_1.p"
-clv_noinput_count_Dx_5_scale_1_data_dir = "data/clv/data_noinput/clv_count_Dx_5_scale_1.p"
-clv_noinput_count_Dx_6_scale_1_data_dir = "data/clv/data_noinput/clv_count_Dx_6_scale_1.p"
-clv_noinput_count_Dx_7_scale_1_data_dir = "data/clv/data_noinput/clv_count_Dx_7_scale_1.p"
-clv_noinput_count_Dx_8_scale_1_data_dir = "data/clv/data_noinput/clv_count_Dx_8_scale_1.p"
-clv_noinput_count_Dx_9_scale_1_data_dir = "data/clv/data_noinput/clv_count_Dx_9_scale_1.p"
-clv_noinput_count_Dx_10_scale_1_data_dir = "data/clv/data_noinput/clv_count_Dx_10_scale_1.p"
-
-############### experiments of sparsity / different observation percentages ############
-clv_count_Dx_6_obs02_data_dir = "data/clv/diff_percentage/clv_count_Dx_6_obsp_02.p"
-clv_count_Dx_6_obs04_data_dir = "data/clv/diff_percentage/clv_count_Dx_6_obsp_04.p"
-clv_count_Dx_6_obs06_data_dir = "data/clv/diff_percentage/clv_count_Dx_6_obsp_06.p"
-
-clv_count_Dx_10_obs02_data_dir = "data/clv/diff_percentage/clv_count_Dx_10_obsp_02.p"
-clv_count_Dx_10_obs04_data_dir = "data/clv/diff_percentage/clv_count_Dx_10_obsp_04.p"
-clv_count_Dx_10_obs06_data_dir = "data/clv/diff_percentage/clv_count_Dx_10_obsp_06.p"
-
-############# experiments of sparsity / different observation percentages, with same account of total observations ####
-clv_count_Dx_10_obs02s_data_dir = "data/clv/compare_noise/clv_count_Dx_10_obsp_02.p"
-clv_count_Dx_10_obs06s_data_dir = "data/clv/compare_noise/clv_count_Dx_10_obsp_06.p"
-clv_count_Dx_10_obs10s_data_dir = "data/clv/compare_noise/clv_count_Dx_10_obsp_10.p"
-
-clv_count_Dx_10_obs02s_no_noise_data_dir = "data/clv/compare_noise/clv_count_Dx_10_obsp_02_no_noise.p"
-clv_count_Dx_10_obs06s_no_noise_data_dir = "data/clv/compare_noise/clv_count_Dx_10_obsp_06_no_noise.p"
-clv_count_Dx_10_obs10s_no_noise_data_dir = "data/clv/compare_noise/clv_count_Dx_10_obsp_10_no_noise.p"
-
+# ----------------------------------------- interpolation data ---------------------------------------- #
 count_clv_interpolation_data_dir = "data/interpolation/count_clv.p"
-simulation_Dx_10_obs_02s_clv_data_dir = "data/interpolation/simulation_Dx_10_obs_02s_clv.p"
-simulation_Dx_10_obs_06s_clv_data_dir = "data/interpolation/simulation_Dx_10_obs_06s_clv.p"
-simulation_Dx_10_obs_02_small_clv_data_dir = "data/interpolation/simulation_Dx_10_obs_02_small_clv.p"
-simulation_Dx_10_obs_06_small_clv_data_dir = "data/interpolation/simulation_Dx_10_obs_06_small_clv.p"
-
-clv_count_Dx_10_obs02_small_data_dir = "data/clv/small/clv_count_Dx_10_obsp_02.p"
-clv_count_Dx_10_obs06_small_data_dir = "data/clv/small/clv_count_Dx_10_obsp_06.p"
-clv_count_Dx_10_obs10_small_data_dir = "data/clv/small/clv_count_Dx_10_obsp_10.p"
 
 PERCENTAGE_DATA_DICT = dict(percentage=percentage_data_dir,
-                            percentage_split_3=percentage_split_3_dir,
-                            percentage_split_5=percentage_split_5_dir,
-                            percentage_small=percentage_small_dir,
-                            clv_percentage_Dx_1_scale_1=clv_percentage_Dx_1_scale_1_data_dir,
-                            clv_percentage_Dx_2_scale_1=clv_percentage_Dx_2_scale_1_data_dir,
-                            clv_percentage_Dx_3_scale_1=clv_percentage_Dx_3_scale_1_data_dir,
-                            clv_percentage_Dx_4_scale_1=clv_percentage_Dx_4_scale_1_data_dir,
-                            clv_percentage_Dx_5_scale_1=clv_percentage_Dx_5_scale_1_data_dir,
-                            clv_percentage_Dx_6_scale_1=clv_percentage_Dx_6_scale_1_data_dir,
-                            clv_percentage_Dx_7_scale_1=clv_percentage_Dx_7_scale_1_data_dir,
-                            clv_percentage_Dx_8_scale_1=clv_percentage_Dx_8_scale_1_data_dir,
-                            clv_percentage_Dx_9_scale_1=clv_percentage_Dx_9_scale_1_data_dir,
-                            clv_percentage_Dx_10_scale_1=clv_percentage_Dx_10_scale_1_data_dir,
-                            clv_percentage_Dx_1_scale_4=clv_percentage_Dx_1_scale_4_data_dir,
-                            clv_percentage_Dx_2_scale_4=clv_percentage_Dx_2_scale_4_data_dir,
-                            clv_percentage_Dx_3_scale_4=clv_percentage_Dx_3_scale_4_data_dir,
-                            clv_percentage_Dx_4_scale_4=clv_percentage_Dx_4_scale_4_data_dir,
-                            clv_percentage_Dx_5_scale_4=clv_percentage_Dx_5_scale_4_data_dir,
-                            clv_percentage_Dx_6_scale_4=clv_percentage_Dx_6_scale_4_data_dir,
-                            clv_percentage_Dx_7_scale_4=clv_percentage_Dx_7_scale_4_data_dir,
-                            clv_percentage_Dx_8_scale_4=clv_percentage_Dx_8_scale_4_data_dir,
-                            clv_percentage_Dx_9_scale_4=clv_percentage_Dx_9_scale_4_data_dir,
-                            clv_percentage_Dx_10_scale_4=clv_percentage_Dx_10_scale_4_data_dir,
-                            clv_noinput_percentage_Dx_1_scale_1=clv_noinput_percentage_Dx_1_scale_1_data_dir,
-                            clv_noinput_percentage_Dx_2_scale_1=clv_noinput_percentage_Dx_2_scale_1_data_dir,
-                            clv_noinput_percentage_Dx_3_scale_1=clv_noinput_percentage_Dx_3_scale_1_data_dir,
-                            clv_noinput_percentage_Dx_4_scale_1=clv_noinput_percentage_Dx_4_scale_1_data_dir,
-                            clv_noinput_percentage_Dx_5_scale_1=clv_noinput_percentage_Dx_5_scale_1_data_dir,
-                            clv_noinput_percentage_Dx_6_scale_1=clv_noinput_percentage_Dx_6_scale_1_data_dir,
-                            clv_noinput_percentage_Dx_7_scale_1=clv_noinput_percentage_Dx_7_scale_1_data_dir,
-                            clv_noinput_percentage_Dx_8_scale_1=clv_noinput_percentage_Dx_8_scale_1_data_dir,
-                            clv_noinput_percentage_Dx_9_scale_1=clv_noinput_percentage_Dx_9_scale_1_data_dir,
-                            clv_noinput_percentage_Dx_10_scale_1=clv_noinput_percentage_Dx_10_scale_1_data_dir
+                            percentage_small=percentage_small_dir
                             )
 
 
@@ -160,62 +30,14 @@ COUNT_DATA_DICT = dict(count=count_data_dir,
                        count_k2=count_data_k2_dir,
                        count_k6=count_data_k6_dir,
                        count_k8=count_data_k8_dir,
-                       count_split_3=count_split_3_dir,
-                       count_split_5=count_split_5_dir,
                        count_small=count_small_dir,
-                       clv_count_Dx_1_scale_1=clv_count_Dx_1_scale_1_data_dir,
-                       clv_count_Dx_2_scale_1=clv_count_Dx_2_scale_1_data_dir,
-                       clv_count_Dx_3_scale_1=clv_count_Dx_3_scale_1_data_dir,
-                       clv_count_Dx_4_scale_1=clv_count_Dx_4_scale_1_data_dir,
-                       clv_count_Dx_5_scale_1=clv_count_Dx_5_scale_1_data_dir,
-                       clv_count_Dx_6_scale_1=clv_count_Dx_6_scale_1_data_dir,
-                       clv_count_Dx_7_scale_1=clv_count_Dx_7_scale_1_data_dir,
-                       clv_count_Dx_8_scale_1=clv_count_Dx_8_scale_1_data_dir,
-                       clv_count_Dx_9_scale_1=clv_count_Dx_9_scale_1_data_dir,
-                       clv_count_Dx_10_scale_1=clv_count_Dx_10_scale_1_data_dir,
-                       clv_count_Dx_1_scale_4=clv_count_Dx_1_scale_4_data_dir,
-                       clv_count_Dx_2_scale_4=clv_count_Dx_2_scale_4_data_dir,
-                       clv_count_Dx_3_scale_4=clv_count_Dx_3_scale_4_data_dir,
-                       clv_count_Dx_4_scale_4=clv_count_Dx_4_scale_4_data_dir,
-                       clv_count_Dx_5_scale_4=clv_count_Dx_5_scale_4_data_dir,
-                       clv_count_Dx_6_scale_4=clv_count_Dx_6_scale_4_data_dir,
-                       clv_count_Dx_7_scale_4=clv_count_Dx_7_scale_4_data_dir,
-                       clv_count_Dx_8_scale_4=clv_count_Dx_8_scale_4_data_dir,
-                       clv_count_Dx_9_scale_4=clv_count_Dx_9_scale_4_data_dir,
-                       clv_count_Dx_10_scale_4=clv_count_Dx_10_scale_4_data_dir,
-                       clv_noinput_count_Dx_1_scale_1=clv_noinput_count_Dx_1_scale_1_data_dir,
-                       clv_noinput_count_Dx_2_scale_1=clv_noinput_count_Dx_2_scale_1_data_dir,
-                       clv_noinput_count_Dx_3_scale_1=clv_noinput_count_Dx_3_scale_1_data_dir,
-                       clv_noinput_count_Dx_4_scale_1=clv_noinput_count_Dx_4_scale_1_data_dir,
-                       clv_noinput_count_Dx_5_scale_1=clv_noinput_count_Dx_5_scale_1_data_dir,
-                       clv_noinput_count_Dx_6_scale_1=clv_noinput_count_Dx_6_scale_1_data_dir,
-                       clv_noinput_count_Dx_7_scale_1=clv_noinput_count_Dx_7_scale_1_data_dir,
-                       clv_noinput_count_Dx_8_scale_1=clv_noinput_count_Dx_8_scale_1_data_dir,
-                       clv_noinput_count_Dx_9_scale_1=clv_noinput_count_Dx_9_scale_1_data_dir,
-                       clv_noinput_count_Dx_10_scale_1=clv_noinput_count_Dx_10_scale_1_data_dir,
-                       clv_count_Dx_6_obs_02=clv_count_Dx_6_obs02_data_dir,
-                       clv_count_Dx_6_obs_04=clv_count_Dx_6_obs04_data_dir,
-                       clv_count_Dx_6_obs_06=clv_count_Dx_6_obs06_data_dir,
-                       clv_count_Dx_10_obs_02=clv_count_Dx_10_obs02_data_dir,
-                       clv_count_Dx_10_obs_04=clv_count_Dx_10_obs04_data_dir,
-                       clv_count_Dx_10_obs_06=clv_count_Dx_10_obs06_data_dir,
-                       clv_count_Dx_10_obs_02s=clv_count_Dx_10_obs02s_data_dir,
-                       clv_count_Dx_10_obs_06s=clv_count_Dx_10_obs06s_data_dir,
-                       clv_count_Dx_10_obs_10s=clv_count_Dx_10_obs10s_data_dir,
-                       clv_count_Dx_10_obs_02s_no_noise=clv_count_Dx_10_obs02s_no_noise_data_dir,
-                       clv_count_Dx_10_obs_06s_no_noise=clv_count_Dx_10_obs06s_no_noise_data_dir,
-                       clv_count_Dx_10_obs_10s_no_noise=clv_count_Dx_10_obs10s_no_noise_data_dir,
-                       clv_count_Dx_10_obs_02_small=clv_count_Dx_10_obs02_small_data_dir,
-                       clv_count_Dx_10_obs_06_small=clv_count_Dx_10_obs06_small_data_dir,
-                       clv_count_Dx_10_obs_10_small=clv_count_Dx_10_obs10_small_data_dir
+                       clv_count_Dx10_obs02=clv_count_Dx10_obs02_dir,
+                       clv_count_Dx10_obs06=clv_count_Dx10_obs06_dir,
+                       clv_count_Dx10_obs10=clv_count_Dx10_obs10_dir,
                        )
 
 
-INTERPOLATION_DATA_DICT = dict(count_clv=count_clv_interpolation_data_dir,
-                               simulation_Dx_10_obs_02s_clv=simulation_Dx_10_obs_02s_clv_data_dir,
-                               simulation_Dx_10_obs_06s_clv=simulation_Dx_10_obs_06s_clv_data_dir,
-                               simulation_Dx_10_obs_02_small_clv=simulation_Dx_10_obs_02_small_clv_data_dir,
-                               simulation_Dx_10_obs_06_small_clv=simulation_Dx_10_obs_06_small_clv_data_dir)
+INTERPOLATION_DATA_DICT = dict(count_clv=count_clv_interpolation_data_dir,)
 
 DATA_DIR_DICT = {**PERCENTAGE_DATA_DICT, **COUNT_DATA_DICT}
 DATA_DIR_DICT["toy"] = toy_data_dir
